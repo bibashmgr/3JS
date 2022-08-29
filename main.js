8c53476633b002fc4286febee4cdb3435ed597dc
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-08-29 15:37:10 +0545

    Updating

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
+import * as THREE from 'three';
+
+// Custom-styling
 import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    Hello
-  </div>
-`;
+// We need three things: scene, camera and renderer, so that we can render the scene with camera.
+
+// 1. Scene
+const scene = new THREE.Scene();
+
+// 2. Camera
+// There are a few different cameras in three.js. For now, let's use a PerspectiveCamera. It takes four parameters: field of view, aspect ratio, near cliping plane and far clipping plane.
+
+// Creating an camera instance
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+// 3. Renderer
+
+// Creating a renderer instance
+const renderer = new THREE.WebGLRenderer();
+
+// Setting the size at which it will render the app
+renderer.setSize(window.innerWidth, window.innerHeight);
