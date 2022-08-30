598fb1b3ceecf012db6d10c521d1bbf068c6899a
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-08-30 20:50:58 +0545

    Updating

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ import './style.css';
 // We need three things: scene, camera and renderer, so that we can render the scene with camera.
 
 // 1. Scene
+// Creating an scene instance
 const scene = new THREE.Scene();
 
 // 2. Camera
@@ -18,11 +19,35 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+// Adjusting camera position
+camera.position.z = 5;
 
 // 3. Renderer
-
 // Creating a renderer instance
 const renderer = new THREE.WebGLRenderer();
 
 // Setting the size at which it will render the app
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Appending to HTML body
+document.body.appendChild(renderer.domElement);
+
+// Creating a cube
+const geometry = new THREE.BoxGeometry(1, 1, 1); // modeling
+const material = new THREE.MeshBasicMaterial({ color: 0xf75aa8 }); // texturing
+const cube = new THREE.Mesh(geometry, material);
+
+// Adding cube to the scene
+scene.add(cube);
+
+// A function to animate the scene
+const animate = () => {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+animate();
