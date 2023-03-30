a6e8fe7ed4da3e7c8312cc5b43376b580321b3c1
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-03-30 19:14:17 +0545

    [update]: shaders

--- src/shaders/fragment_shader.glsl
+++ src/shaders/fragment_shader.glsl
@@ -1,3 +1,16 @@
+varying vec2 vUv;
+varying float vWave;
+uniform sampler2D uTexture;
+
 void main() {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    float wave = vWave * 0.2;
+
+    vec3 texture = texture2D(uTexture, vUv + wave).rgb;
+
+    // float r = texture2D(uTexture, vUv).r;
+    // float g = texture2D(uTexture, vUv).g;
+    // float b = texture2D(uTexture, vUv).b;
+    // vec3 texture = vec3(r, g, b);
+
+    gl_FragColor = vec4(texture, 1.0);
 }
